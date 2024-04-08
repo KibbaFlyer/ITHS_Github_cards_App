8769691f65b6e6f3539043994b20fa0d7350fb0e
KibbaFlyer <kristoffer.flygare@icloud.com>
2024-04-08 15:38:40 +0200

    Done! Testproject finished

--- GitHub Cards App/src/App.jsx
+++ GitHub Cards App/src/App.jsx
@@ -1,35 +1,79 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
+import axios from 'axios'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const testData = [
+  {name: "Dan Abramov", avatar_url: "https://avatars0.githubusercontent.com/u/810438?v=4", company: "@facebook"},
+  {name: "Sophie Alpert", avatar_url: "https://avatars2.githubusercontent.com/u/6820?v=4", company: "Humu"},
+  {name: "Sebastian Markbåge", avatar_url: "https://avatars2.githubusercontent.com/u/63648?v=4", company: "Facebook"},
+];
 
-  return (
-    <>
+const CardList = (props) => (
+  <div>
+    {props.profiles.map(profile => <Card key={profile.Id} {...profile}/>)}
+  </div>
+);
+
+class Card extends React.Component {
+  render (){
+    const profile = this.props;
+    return (
+      <div className="github-profile">
+        <img src={profile.avatar_url} />
+        <div className="info">
+          <div className="name">{profile.name}</div>
+          <div className="company">{profile.company}</div>
+        </div>
+      </div>
+    )
+  }
+}
+
+class Form extends React.Component {
+  state = {userName: ''};
+  handleSubmit = async (event) => {
+    event.preventDefault();
+    const response = await axios.get(`https://api.github.com/users/${this.state.userName}`)
+    this.props.onSubmit(response.data);
+    this.setState({userName: ''});
+  }
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input 
+        type="text" 
+        placeholder="GitHub username" 
+        onChange={event => this.setState({userName: event.target.value})}
+        value={this.state.userName}
+        required/>
+        <button>Add card</button>
+      </form>
+    )
+  }
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      profiles: testData
+    };
+  }
+  addnewProfile = (profileData) => {
+    this.setState(prevState => ({
+      profiles: [...prevState.profiles, profileData]
+    }));
+  }
+  render() {
+    return (
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <div className="header">{this.props.title}</div>
+        <Form onSubmit={this.addnewProfile}/>
+        <CardList profiles={this.state.profiles}/>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+
+    )
+  }
 }
 
 export default App
